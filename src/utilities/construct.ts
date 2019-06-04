955aa6a8d4b1213cb8bc9b6d56c2c5f8b20a7470
Michele Dexter <micheleannedexter@gmail.com>
2019-06-04 08:41:10 -0400

    add additional converters

--- src/utilities/construct.ts
+++ src/utilities/construct.ts
@@ -1,6 +1,7 @@
 import { User } from '../models/user';
 import { Role } from '../models/role';
 import cookies from 'js-cookie';
+import { Reimbursement, ReimbursementStatus, ReimbursementType, FullReimbursement } from '../models/reimbursement';
 
 export const getUser = () => {
   let user: User;
@@ -13,4 +14,31 @@ export const getUser = () => {
   }
 
   return user
+}
+
+export const fullReimbursement = (reimbursement: Reimbursement, statuses: ReimbursementStatus[], types: ReimbursementType[]): FullReimbursement => {
+  let statusId = reimbursement.status;
+  let typeId = reimbursement.type;
+  let status = new ReimbursementStatus(-1, '');
+  let type = new ReimbursementType(-1, '');
+  for (let s of statuses) {
+    if (s.statusId === statusId) {
+      status = s;
+    }
+  }
+  for (let t of types) {
+    if (t.typeId === typeId) {
+      type = t;
+    }
+  }
+  return new FullReimbursement(reimbursement.reimbursementId, reimbursement.author, reimbursement.amount, reimbursement.dateSubmitted, reimbursement.dateResolved, reimbursement.description, reimbursement.resolver, status, type);
+}
+
+export function epochDateToStringDate(date: number): string {
+    let fullDate = new Date(date);
+    return (fullDate.getMonth()+1) + '/' + fullDate.getDate() + '/' + fullDate.getFullYear();
+}
+
+export function stringDateToEpochDate(date: string): number {
+    return new Date(date).getTime();
 }
