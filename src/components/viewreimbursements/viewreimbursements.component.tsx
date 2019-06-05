42b84f38ebb3f5917e6ad5779b9901fc698b8ba8
Michele Dexter <micheleannedexter@gmail.com>
2019-06-04 22:20:40 -0400

    add basic search functionality

--- src/components/viewreimbursements/viewreimbursements.component.tsx
+++ src/components/viewreimbursements/viewreimbursements.component.tsx
@@ -7,9 +7,11 @@ import { apiClient } from '../../axios/user-api-client';
 import { FullReimbursement, Reimbursement, ReimbursementStatus, ReimbursementType } from '../../models/reimbursement';
 import { fullReimbursement } from '../../utilities/construct';
 import ReimbursementCard from '../reimbursementcard/reimbursementcard.component';
+import { checkUserPermission } from '../../utilities/handle';
 
 interface IViewReimbursementsState {
   reimbursements: FullReimbursement[];
+  dropdown: JSX.Element;
 }
 
 interface IViewReimbursementsProps {
@@ -17,13 +19,15 @@ interface IViewReimbursementsProps {
   history: History;
   statuses: ReimbursementStatus[];
   types: ReimbursementType[];
+  users: User[];
 }
 
 class ViewReimbursements extends Component<IViewReimbursementsProps, IViewReimbursementsState> {
   constructor(props: IViewReimbursementsProps) {
     super(props);
     this.state = {
-      reimbursements: []
+      reimbursements: [],
+      dropdown: <select></select>
     }
   }
 
@@ -47,13 +51,43 @@ class ViewReimbursements extends Component<IViewReimbursementsProps, IViewReimbu
     });
   }
 
+  setStatusDropdown = () => {
+    let dropdown = <select defaultValue="0">
+      <option value="0">-- Select a status --</option>
+      {this.props.statuses.map((status) => {
+        return <option key={"status-" + status.statusId} value={status.statusId} onClick={() => this.getReimbursementsByStatusId(status.statusId)}>{status.status}</option>
+      })}
+    </select>;
+    this.setState({dropdown});
+  }
+
+  setAuthorDropdown = () => {
+    let dropdown = <select defaultValue="0">
+      <option value="0">-- Select an author --</option>
+      {this.props.users.map((user) => {
+        return <option key={"author-" + user.userId} value={user.userId} onClick={() => this.getReimbursementsByAuthorId(user.userId)}>{user.firstName + " " + user.lastName}</option>
+      })}
+    </select>;
+    this.setState({dropdown});
+  }
+
   componentDidMount() {
-    this.getReimbursementsByStatusId(3);
+    checkUserPermission(this.props.history, this.props.currentUser.role.role, ['admin', 'finance-manager']);
+    this.getReimbursementsByStatusId(1);
+    this.setStatusDropdown();
   }
 
   render() {
     return (
       <div className="ViewReimbursements">
+        <div className="reimbursement-select">
+          <select defaultValue="1">
+            <option value="1" onClick={this.setStatusDropdown}>Search by status</option>
+            <option value="2" onClick={this.setAuthorDropdown}>Search by author</option>
+          </select>
+          &nbsp;
+          {this.state.dropdown}
+        </div>
         {this.state.reimbursements.map((item) => <ReimbursementCard key={"reimbursement" + item.reimbursementId} reimbursement={item} />)}
       </div>
     );
@@ -64,7 +98,8 @@ const mapStateToProps = (state: IState) => {
   return {
     currentUser: state.login.currentUser,
     statuses: state.reimbursement.statuses,
-    types: state.reimbursement.types
+    types: state.reimbursement.types,
+    users: state.user.users
   }
 }
 
