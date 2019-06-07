b306f8d0462808afe683c5f947a27d388d7402ca
Michele Dexter <micheleannedexter@gmail.com>
2019-06-07 14:25:12 -0400

    remove reference to localhost

--- src/axios/user-api-client.ts
+++ src/axios/user-api-client.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export const apiClient = axios.create({
   baseURL: 'http://ec2-18-220-194-68.us-east-2.compute.amazonaws.com:5000',
-  // baseURL: 'http://localhost:5000',
   headers: {
     'Content-Type': 'application/json'
   },
