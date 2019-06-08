9a5252209aac3986550f9e350252cdcdecbfb7cb
Michele Dexter <micheleannedexter@gmail.com>
2019-06-08 13:31:41 -0400

    add filtering to results

--- src/components/viewreimbursements/viewreimbursements.component.tsx
+++ src/components/viewreimbursements/viewreimbursements.component.tsx
@@ -7,10 +7,13 @@ import { FullReimbursement, ReimbursementStatus, ReimbursementType } from '../..
 import ReimbursementCard from '../reimbursementcard/reimbursementcard.component';
 import { checkUserPermission } from '../../utilities/handle';
 import { fetchReimbursementsByStatusId, fetchReimbursementsByAuthorId } from '../../actions/reimbursement.actions';
+import { epochDateToStringDate } from '../../utilities/convert';
+import './viewreimbursements.component.css';
 
 interface IViewReimbursementsState {
   dropdown: JSX.Element;
   sort: string;
+  search: string;
 }
 
 interface IViewReimbursementsProps {
@@ -28,13 +31,28 @@ class ViewReimbursements extends Component<IViewReimbursementsProps, IViewReimbu
   constructor(props: IViewReimbursementsProps) {
     super(props);
     this.state = {
-      dropdown: <select></select>,
-      sort: 'status'
+      dropdown: <select className="form-control"></select>,
+      sort: 'status',
+      search: '',
     }
   }
 
+  getNameById = (id: number) => {
+    if (id === null || id <= 0) return "No Resolver";
+    for (let user of this.props.users) {
+      if (id === user.userId) {
+        return user.firstName + " " + user.lastName;
+      }
+    }
+  }
+
+  getResolveDate = (date: number) => {
+    if (date < 31554000000) return 'Pending';
+    return epochDateToStringDate(date);
+  }
+
   setStatusDropdown = () => {
-    let dropdown = <select defaultValue="0">
+    let dropdown = <select className="form-control" defaultValue="0">
       <option value="0">-- Select a status --</option>
       {this.props.statuses.map((status) => {
         return <option key={"status-" + status.statusId} value={status.statusId} onClick={() => this.props.fetchReimbursementsByStatusId(status.statusId, this.props.statuses, this.props.types)}>{status.status}</option>
@@ -47,7 +65,7 @@ class ViewReimbursements extends Component<IViewReimbursementsProps, IViewReimbu
   }
 
   setAuthorDropdown = () => {
-    let dropdown = <select defaultValue="0">
+    let dropdown = <select className="form-control" defaultValue="0">
       <option value="0">-- Select an author --</option>
       {this.props.users.map((user) => {
         return <option key={"author-" + user.userId} value={user.userId} onClick={() => this.props.fetchReimbursementsByAuthorId(user.userId, this.props.statuses, this.props.types)}>{user.firstName + " " + user.lastName}</option>
@@ -59,6 +77,29 @@ class ViewReimbursements extends Component<IViewReimbursementsProps, IViewReimbu
     });
   }
 
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      search: event.target.value
+    });
+  }
+
+  filter = (input: string): FullReimbursement[] => {
+    const results = this.props.reimbursements.filter(rt => {
+      if (input === '') return rt;
+      let re = '.*' + input.toLowerCase() + '.*';
+      let regexp = new RegExp(re);
+      return (('$' + rt.amount.toFixed(2)).match(regexp) || 
+        this.getNameById(rt.author).toLowerCase().match(regexp) ||
+        this.getResolveDate(rt.dateResolved).toLowerCase().match(regexp) ||
+        epochDateToStringDate(rt.dateSubmitted).toLowerCase().match(regexp) ||
+        rt.description.toLowerCase().match(regexp) ||
+        this.getNameById(rt.resolver).toLowerCase().match(regexp) ||
+        rt.status.status.toLowerCase().match(regexp) ||
+        rt.type.type.toLowerCase().match(regexp));
+    });
+    return results;
+  }
+
   componentDidMount() {
     checkUserPermission(this.props.history, this.props.currentUser.role.role, ['admin', 'finance-manager']);
     this.props.fetchReimbursementsByStatusId(1, this.props.statuses, this.props.types);
@@ -68,15 +109,17 @@ class ViewReimbursements extends Component<IViewReimbursementsProps, IViewReimbu
   render() {
     return (
       <div className="ViewReimbursements">
-        <div className="reimbursement-select">
-          <select defaultValue="1">
+        <div className="form-group reimbursement-select">
+          <select className="form-control" defaultValue="1">
             <option value="1" onClick={this.setStatusDropdown}>Search by status</option>
             <option value="2" onClick={this.setAuthorDropdown}>Search by author</option>
           </select>
           &nbsp;
           {this.state.dropdown}
+          &nbsp;
+          <input className="form-control" value={this.state.search} onChange={this.handleChange} placeholder="Filter results" />
         </div>
-        {this.props.reimbursements.map((item) => <ReimbursementCard key={"reimbursement" + item.reimbursementId} history = {this.props.history} reimbursement={item} sort={this.state.sort} />)}
+        {this.filter(this.state.search).map((item) => <ReimbursementCard key={"reimbursement" + item.reimbursementId} history = {this.props.history} reimbursement={item} sort={this.state.sort} />)}
       </div>
     );
   }
